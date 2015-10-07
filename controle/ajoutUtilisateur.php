12ae98a9cb1aa64f8ac28559d574df14e64afdab
YRPRO <YRPRO@users.noreply.github.com>
2015-10-07 09:13:57 +0200

    Update ajoutUtilisateur.php

--- controle/ajoutUtilisateur.php
+++ controle/ajoutUtilisateur.php
@@ -1,43 +1,49 @@
 
-
-
 <?php
-
+require('fonction.php');
 
 if(isset($_POST['valide'])){
 
 	if(isset($_POST['login'])&& isset($_POST['nom']) && isset($_POST['prenom'])&& isset($_POST['jour'])&& isset($_POST['mois'])&& isset($_POST['annee'])
 		&& isset($_POST['email'])&& isset($_POST['pwd'])&& isset($_POST['pwd_confirm'])) 
 	{
+$errors=[];
+	extract($_POST); 
+					
+	if(mb_strlen($login) > 15 ){
+$errors[]="Votre login est trop long (Maximum 15 caractères";
+}
+if(mb_strlen($login)<3){
+$errors[]="Votre login est trop court (Minimum 3 caractères";
+}	
 
-					extract($_POST); // Pour eviter de reecrire a chaque fois les affectation 
-					if($pwd != $pwd_confirm){
-                     /* {Redirection de l'user vers la page d'accueil .*/
-					echo 'Votre mot de passe n\'est pas valide,vous aller etre rediriger dans un instant.';
-		            header("Refresh: 1;URL=accueil.html");	
-				} else {
-                      session_start();
+/*-----------A activer a la fin du projet sinon c la merdasse pour les test
+if (filter_var($email,FILTER_VALIDATE_EMAIL)){
+	$errors="Votre adresse email n'est pas valide";
+}*/
+		if(mb_strlen($pwd)<6){
+$errors[]="Votre mot de passe est trop court (Minmum 3 caractères)";
+}else {
+	if($pwd != $pwd_confirm){
+		$errors[]="Les deux mot de passe ne sont pas identiques";
+	}
+	}	
+
+		if (count($errors)==0) {
+                    	session_start();
 		              $_SESSION['nom'] = $nom;
 		              $_SESSION['prenom'] = $prenom;
-
+                      
                     Inscription($login,$nom,$prenom,$sexe,$jour,$mois,$annee,$email,$pwd);
+
 				}
-					}
-	}
-		function Inscription ($login,$nom, $prenom,$sexe,$jour,$mois,$annee,$email,$pwd) {
-		require ("../modele/dataBase.php");
-		$dateN= $annee.'-'.$mois.'-'.$annee; // On stock dans ce format parceque c pour le type date de la base de donnée 
-		$dateInscription = date("Y-d-m");
-		$req = $db->prepare('INSERT INTO utilisateur(login, nom, prenom,email, password, dateInscription,dateN, sexe) VALUES(:login,:nom, :prenom, :email, :password,:dateInscription , :dateN, :sexe)');
-$req->execute(array('login'=> $login,
-					 'nom' => $nom,
-					 'prenom' => $prenom,
-					 'email' => $email,
-					 'password' => $pwd,
-					 'dateInscription' => $dateInscription,
-					 'dateN' => $dateN,
-					 'sexe' => $sexe,
-					 ));
-echo 'reussi';
+	
+}else {
+	$errors[] = "Veuillez remplir tout les champs pour vous inscrire";
 }
+			
+	
+				}
+				require('../vue/inscription.php');
+				
 ?>
